b131d26e368ee7a8ca7dbc02ad4dcc7916456bcb
= <=>
2020-05-22 15:46:54 +0200

    add design pattern project

---
+++ Creational/Singleton.py
@@ -0,0 +1,25 @@
+"""
+    Creational:
+        Singleton
+"""
+
+class Singleton(type):
+    _instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self._instance==None:
+            self._instance= super().__call__()
+        return self._instance
+
+
+
+class Db(metaclass=Singleton):
+    pass
+
+d1 = Db()
+d2 = Db()
+d3 = Db()
+
+print(id(d1))
+print(id(d2))
+print(id(d3))
