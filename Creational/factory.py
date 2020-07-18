5d89eca35e7ccf06bfac6c65cc5d12ae2a5dd5f5
= <=>
2020-07-19 01:43:37 +0200

    update factory design pattern

--- Creational/factory.py
+++ Creational/factory.py
@@ -1,24 +1,24 @@
 # befor 
-# class FormatDefine:
-#     def __init__(self, name, format):
-#         self.name = name
-#         self.format = format
+class FormatDefine:
+    def __init__(self, name, format):
+        self.name = name
+        self.format = format
 
-# class Editing:
-#     def edit(self, file):
-#         if file.format == 'pdf':
-#             print(f'Editing pdf {file.name}....')
-#         elif file.format == 'xml':
-#             print(f'Editing Xml {file.name}...')
-#         else:
-#             raise ValueError('format not exist...')
-
-
-# format = FormatDefine('myFile', 'pdf')
-# editing = Editing()
-# editing.edit(format)
+class Editing:
+    def edit(self, file):
+        if file.format == 'pdf':
+            print(f'Editing pdf {file.name}....')
+        elif file.format == 'xml':
+            print(f'Editing Xml {file.name}...')
+        else:
+            raise ValueError('format not exist...')
 
 
+format = FormatDefine('myFile', 'pdf')
+editing = Editing()
+editing.edit(format)
+
+#----------------------------------------------------------------------------
 
 # after
 # Factory Design Pattern 
@@ -54,4 +54,46 @@ class Editing:
 format = FormatDefine('myFile', 'pdf')
 editing = Editing()
 editing.edit(format)
+
+#----------------------------------------------------------------------------
+
+from abc import ABC,abstractclassmethod
+
+class Creator(ABC):
+    @abstractclassmethod
+    def make(self):
+        pass
+
+    def call_edit(self):
+        product = self.make()
+        result = product.edit()
+        return result
+
+class JsonCreator(Creator):
+    def make(self):
+        return Json()
+
+class PdfCreator(Creator):
+    def make(self):
+        return PDF()
+#------------------------------
+class Product(ABC):
+    @abstractclassmethod
+    def edit(self):
+        pass
+
+class Json(Product):
+    def edit(self):
+        return f'Editing Json file....'
+
+class PDF(Product):
+    def edit(self):
+        return f'Editing Pdf file......'
+#------------------------------
+def client(format):
+    return format.call_edit()
+
+print(client(JsonCreator()))
+
+
 
