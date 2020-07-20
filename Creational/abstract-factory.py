5737c18a55b50f0059e7ca7b5375d6e348be04fc
= <=>
2020-07-20 11:02:27 +0200

    edit abstract factory design pattern sanple

--- Creational/abstract-factory.py
+++ Creational/abstract-factory.py
@@ -1,47 +1,74 @@
 """
-    Factory design pattern with abstract 
-    
-    1- creator
-    2- product 
-    3- client
+	Abstract Factory
+		Car => Benz, Bmw => Suv, Coupe
+			benz suv => gla, glc
+			bmw suv => x1, x2
+			benz coupe => cls, E-class
+			bmw coupe => m2, m4
 """
+from abc import ABC,abstractclassmethod
 
-from abc import ABC,abstractmethod
-
-#-------------------------------------------
-class Creator(ABC):
-    @abstractmethod
-    def make(self):
+class Car(ABC):
+    @abstractclassmethod
+    def call_suv(self):
         pass
 
-    def call_edit(self):
-        procuct = self.make()
-        result = procuct.edit()
-        return result
-
-class JsonCreator(Creator):
-    def make(self):
-        return json()
-
-class XmlCreator(Creator):
-    def make(self):
-        return xml()
-#-------------------------------------------
-class Procuct(ABC):
-    @abstractmethod
-    def edit(self):
+    @abstractclassmethod
+    def call_coupe(self):
+        pass
+#---------------------------------------------
+class Benz(Car):
+    def call_suv(self):
+        return Gla()
+    def call_coupe(self):
+        return Cls()
+#---------------------------------------------
+class Bmw(Car):
+    def call_suv(self):
+        return X1()
+    def call_coupe(self):
+        return M2()
+#---------------------------------------------
+class SUV(ABC):
+    @abstractclassmethod
+    def create_suv(self):
         pass
 
-class json(Procuct):
-    def edit(self):
-        return 'the file is json'
+class Coupe(ABC):
+    @abstractclassmethod
+    def create_coupe(self):
+        pass
+#------------------------------------------------
+        # Benz
+class Gla(SUV):
+    def create_suv(self):
+        print("this is your Gla SUV Benz...")
 
-class xml(Procuct):
-    def edit(self):
-        return 'the file is xml'
-#-------------------------------------------
-def client(format):
-    return format.call_edit()
+class Cls(Coupe):
+    def create_coupe(self):
+        print("this is your cls coupe Benz...")
+#---------------------------------------------------
+    # BMW
+class X1(SUV):
+    def create_suv(self):
+        print("this is your X1 SUV BMW .... ")
 
+class M2(Coupe):
+    def create_coupe(self):
+        print("this is your me coupe BMW ....")
+#------------------------------------------------------
 
-print(client(XmlCreator()))
+def client_suv_order(order):
+    suv = order.call_suv()
+    suv.create_suv()
+
+def client_coupe_order(order):
+    coupe= order.call_coupe()
+    coupe.create_coupe()
+
+#----------------------------------------------------------
+client_coupe_order(Benz())
+client_coupe_order(Bmw())
+
+client_suv_order(Benz())
+client_suv_order(Bmw())
